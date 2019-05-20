176f0ff46adc32a247a02c5cca437dc233fedc85
Erin Ponsonby <erinponsonby@gmail.com>
2019-05-20 21:13:59 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,8 +1,9 @@
 require 'pry'
 
 class Artist
-  extend Memorable
+  extend Memorable::ClassMethods
   include Paramable
+  include Memorable::InstanceMethods
   
   attr_accessor :name
   attr_reader :songs
