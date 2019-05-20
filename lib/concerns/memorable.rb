69b8facb2b6253364fd11bbb323ee9c04e5bb3ea
Erin Ponsonby <erinponsonby@gmail.com>
2019-05-20 21:12:40 +0000

    Automatically backed up by Learn

--- lib/concerns/memorable.rb
+++ lib/concerns/memorable.rb
@@ -1,4 +1,5 @@
 module Memorable
+  module ClassMethods
   
   def reset_all
     self.all.clear
@@ -6,6 +7,12 @@ module Memorable
   
   def count
     self.all.count
+  end
+  
+  module InstanceMethods
+    def initialize
+      self.class.all << self
+    end
   end
     
 end
